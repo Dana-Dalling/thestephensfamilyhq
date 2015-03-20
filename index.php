3814a41af259c76e8c540bf2171f953d934b7d61
Dana Dalling <dana@octobits.io>
2015-03-20 13:44:23 -0500

    Update home page layout, Add location images, Add font Awesome icon library

---
+++ index.php
@@ -0,0 +1,163 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Dana & Fabion are getting married! | Welcome to our website!</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+    <link rel="stylesheet" href="css/main.css" />
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+    <div class="hero-bg">
+        <div class="header-container">
+            <div class="row" id="nav">
+                <div class="small-12 medium-4 large-4 columns">
+                    <div class="row">
+                        <ul class="small-block-grid-3">
+                            <li><a>RSVP</a></li>
+                            <li><a>Venue</a></li>
+                            <li><a>Suggest Songs</a></li>
+                         </ul>
+                    </div>
+                </div>
+                <div class="small-12 medium-4 large-4 columns logo-container">
+                    <img src="img/logo.png" title="Dana and Fabion">
+                </div>
+                <div class="small-12 medium-4 large-4 columns">
+                    <ul class="small-block-grid-2">
+                        <li><a>Guest Book</a></li>
+                        <li><a>Wedding Album</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="small-11 large-centered columns text-center you-are-invited">
+                    <h1>We’re getting married & You’re invited!</h1>
+                    <p>Please join us for our wedding celebrations at Straun Castle Garden,<br>
+                        Stony Hill, St. Andrew. Ceremony begins @ 5pm.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+       <div class="small-12 large-centered columns text-center rsvp-cta">
+           <p>We are so glad that you are coming! Please RSVP by June 14, 2015
+               <a href="#" class="button tiny radius rsvp-btn">RSVP now</a>
+           </p>
+       </div>
+    </div>
+    <div class="row">
+        <div class="small-12 medium-6 large-6 columns text-center the-couple">
+
+            <h4 class="text-center">
+                <i class="fa fa-heart"></i>
+                <i class="fa fa-heart"></i>
+                <i class="fa fa-heart"></i>
+                The Bride
+                <i class="fa fa-heart"></i>
+                <i class="fa fa-heart"></i>
+                <i class="fa fa-heart"></i>
+            </h4>
+            <img src="img/dana.png">
+            <div class="name">
+                Dana Deandra Dalling
+            </div>
+            <p class="about">
+                Tmet adipiscing sem neque
+                sed ipsum. Nam quam nunc, blandit vel, luctus
+                pulvinar, hendrerit id, lorem. Maecenas nec
+                odio e ante tincidunt tempus. Donec vitae sapien
+                ut libero venenatis faucibus.
+            </p>
+        </div>
+        <div class="small-12 medium-6 large-6 columns text-center the-couple">
+            <h4 class="text-center">
+                <i class="fa fa-heart"></i>
+                <i class="fa fa-heart"></i>
+                <i class="fa fa-heart"></i>
+                The Groom
+                <i class="fa fa-heart"></i>
+                <i class="fa fa-heart"></i>
+                <i class="fa fa-heart"></i>
+            </h4>
+            <img src="img/fabion.png">
+            <div class="name">
+                Fabion O'brian Stephens
+            </div>
+            <p class="about">Maecenas tempus, tellus eget condimentum rhoncus,
+                sem quam semper libero, sit amet adipiscing sem neque
+                odio e ante tincidunt tempus. Donec vitae sapien
+                ut libero venenatis faucibus.</p>
+        </div>
+        <div class="small-12 medium-12 large-12 columns text-center ">
+            <img src="img/read-our-story-cta.png" class="our-story-img">
+        </div>
+    </div>
+    <div class="row">
+        <div class="small-12 medium-12 large-12 columns text-center our-bridal-party-heading">
+            <h5>Our Bridal Party</h5>
+        </div>
+    </div>
+    <div class="row ">
+        <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4 text-center our-bridal-party mb3">
+            <li>
+                <img src="img/latoya.png">
+                <h3>Latoya Dalling</h3>
+                <h6>Bridesmaid</h6>
+                <p> Sister of the Bride luctus
+                    pulvinar, hendrerit id, lorem. Maecenas nec
+                </p>
+            </li>
+           <li>
+               <img src="img/iana.png">
+               <h3>Iana Cross</h3>
+               <h6>Bridesmaid</h6>
+               <p> Sister of the Bride luctus
+                   pulvinar, hendrerit id, lorem. Maecenas nec
+               </p>
+           </li>
+            <li>
+                <img src="img/kwami.png">
+                <h3>Kwami Roberts</h3>
+                <h6>Best Man</h6>
+                <p> Friend of the Groom,
+                    pulvinar, hendrerit id, lorem. Maecenas nec
+                </p>
+            </li>
+            <li>
+                <img src="img/oliver.png">
+                <h3>Oliver Stephens</h3>
+                <h6>Groomsman</h6>
+                <p> Brother of the Groom
+                    pulvinar, hendrerit id, lorem. Maecenas nec
+                </p>
+            </li>
+        </ul>
+    </div>
+    <div class="footer">
+        <div class="row">
+            <div class="small-12 small-centered medium-6 medium-centered large-6 large-centered columns">
+                <p>
+                    <i class="fa fa-heart"></i>
+                    <i class="fa fa-heart"></i>
+                    © 2015 All Rights Reserved.
+                     Made with Love by <a href="http://www.octobits.io">Dana & Fabion</a>
+                    <i class="fa fa-heart"></i>
+                    <i class="fa fa-heart"></i>
+
+                </p>
+
+            </div>
+        </div>
+    </div>
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
